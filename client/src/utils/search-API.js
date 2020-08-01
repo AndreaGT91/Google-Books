ca15601bf973159e344763f73ce0cc38e0a4f59a
Andrea Bentley <andrea@bentleytech.com>
2020-08-01 16:56:17 -0400

    Added Google Books API call

--- client/src/utils/search-API.js
+++ client/src/utils/search-API.js
@@ -1,20 +1,10 @@
 import axios from "axios";
+require("dotenv").config();
 
-export default {
-  // Gets all books
-  getBooks: function() {
-    return axios.get("/api/books");
-  },
-  // Gets the book with the given id
-  getBook: function(id) {
-    return axios.get("/api/books/" + id);
-  },
-  // Deletes the book with the given id
-  deleteBook: function(id) {
-    return axios.delete("/api/books/" + id);
-  },
-  // Saves a book to the database
-  saveBook: function(bookData) {
-    return axios.post("/api/books", bookData);
-  }
+function getGoogleBooks(keywords) {
+  // Replace spaces in array with + sign
+  const keyword = keywords.replace(/\s/g, "+");
+  return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${keyword}&key=${process.env.REACT_APP_GOOGLE_KEY}`);
 };
+
+export default getGoogleBooks;
