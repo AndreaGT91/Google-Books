c952e0bfcb7d86b57c6fa17b6ebf12d51aba392d
Andrea Bentley <andrea@bentleytech.com>
2020-07-31 23:11:14 -0400

    Added header and search form to Search page

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -4,6 +4,26 @@ import Wrapper from "../components/Wrapper";
 function Search() {
   return (
     <Wrapper>
+      <div className="jumbotron jumbotron-fluid" style={{ marginTop: "80px"}}>
+        <div className="container text-center">
+          <h1 className="display-4">(React) Google Books Search</h1>
+          <p className="lead">Search for and Save Books of Interest</p>
+        </div>
+      </div>
+      <div className="jumbotron jumbotron-fluid" style={{ marginTop: "20px", paddingTop: "0px", paddingBottom: "20px" }}>
+        <div className="container">
+          <form>
+            <h3 style={{ marginBottom: "20px" }}>Book Search</h3>
+            <div className="form-group">
+              <label for="keywordInput">Keywords:</label>
+              <input type="subit" className="form-control" id="keywordInput" placeholder="Enter keywords to search for" />
+            </div>
+            <div className="form-group">
+              <button type="submit" className="btn btn-primary pull-right">Search</button>
+            </div>
+          </form>
+        </div>
+      </div>
     </Wrapper>
   );
 };
